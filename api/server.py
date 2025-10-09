4f07ded7f4337d307368e2561ac67db6abd142eb
Adil Al-Tameemi <Adil@Adils-MacBook-Pro.local>
2025-10-09 18:05:35 +0400

    Add sandbox credentials for TotalPay

--- api/server.py
+++ api/server.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 import sqlite3
 import os
 
+MERCHANT_KEY = "d57181cc-9f60-11f0-a37e-563fa6bd0e58"
+MERCHANT_PASS = "aafc6a570e8193c5525a7e0c207d05e5"
+
 app = FastAPI()
 
 @app.get("/api/health")
